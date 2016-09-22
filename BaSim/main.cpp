3055c39bd878489595ad16d0f5c1184aa7ab32eb
Manuel Lera Ramirez <Manu@mac-nedelec46.embl.de>
2016-09-22 09:51:47 +0200

    Initial Commit

---
+++ BaSim/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BaSim
+//
+//  Created by Manuel Lera Ramirez on 22/09/16.
+//  Copyright © 2016 Manuel Lera Ramirez. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
