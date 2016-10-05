47e9e73f6a03f6f2d11311cadbb20d8f123a6a82
Manuel Lera Ramirez <Manu@mac-nedelec46.embl.de>
2016-10-05 11:06:51 +0200

    We want to add the correct boundaries to the Mts

--- BaSim/main.cpp
+++ BaSim/main.cpp
@@ -6,10 +6,131 @@
 //  Copyright © 2016 Manuel Lera Ramirez. All rights reserved.
 //
 
-#include <iostream>
+/* BaSim, the simulation that I used to start learning */
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+// Basic Libraries
+#include <iostream>
+#include <sstream>
+
+// Graphic Libraries
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+
+#include <cstdlib>
+#include <cmath>
+float t = 0.0; float t_old=0.0; float dt=0.0;
+
+//float ycorrection = static_cast<float>(yBound)/static_cast<float>(xBound);
+// Import the objects of the simulation
+#include "params.h"
+#include "MT.h"
+#include "ball.h"
+#include "display.h"
+MT mt[ntubs];
+Ball ball[nballs];
+// Initialize internal variables for the simulation
+
+int CountBound()
+{
+    int counter=0;
+    for(int i = 0; i < nballs; i++)
+    {
+        counter+=!!ball[i].attached;
+    }
+    return counter;
+}
+
+
+void display()
+{
+    glClearColor(0.0f, 0.0f,0.0f,1.0f);
+    glfwMakeContextCurrent(graphs);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwMakeContextCurrent(win);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glPointSize(5);
+    
+    //MT
+    for (int j=0; j<ntubs; j++)
+    {
+
+    glLineWidth(2.0f);
+    glBegin(GL_LINES);
+    {
+        glColor3f(0.0, 0.0, 1.0);
+        glVertex2f(mt[j].posx/xBound, mt[j].posy/yBound);
+        glVertex2f(mt[j].posxp()/xBound, mt[j].posyp()/yBound);
+    }
+    glEnd();
+    }
+    
+    
+    //Motors
+    for (int i=0; i<nballs; i++)
+    {
+
+        for (int j=0; j<ntubs; j++)
+        {
+            ball[i].step(mt[j],j);
+        }
+        glBegin(GL_POINTS);
+        {
+            if (!ball[i].attached)
+                {glColor3f(1.0,0.0,0.0);}
+            else
+                {
+                glColor3f(0.0,1.0,0.0);}
+            glVertex2f(ball[i].x/xBound, ball[i].y/yBound);
+        }
+        glEnd();
+        glBegin(GL_POINTS);
+        {
+            glColor3f(1.0,1.0,0.0);
+            glVertex2f(xBound,yBound);
+        }
+        glEnd();
+        
+    }
+    
+
+    // Update the graphs
+    time_vect.push_back(time_vect.at(time_vect.size()-1)+dt);
+    bound_vect.push_back(static_cast<float>(CountBound())/static_cast<float>(nballs));
+    graphsRefresh();
+}
+
+int main() {
+    for (int i=0;i<20;i++) std::cout << srand()<<std::endl;
+    if (!glfwInit())
+    {
+        return -1;
+    }
+    graphs = glfwCreateWindow(xGraphs, yGraphs, "Graphs", NULL, NULL);
+    win = glfwCreateWindow(xBound, yBound, "BaSim", NULL, NULL);
+    if (!win){
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+    if (!glewInit()) return -1;
+    
+        //glEnable(GL_PROGRAM_POINT_SIZE_EXT);
+    //glPointSize(1000);
+    glfwMakeContextCurrent(win);
+    glfwGetFramebufferSize(win, &xBound, &yBound);
+    while (!glfwWindowShouldClose(win)){
+        t = glfwGetTime();
+        dt = t - t_old;
+        t_old = t;
+        display();
+        
+        glfwSwapBuffers(win);
+        glfwPollEvents();
+    }
+    
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
+    
     return 0;
 }
+
